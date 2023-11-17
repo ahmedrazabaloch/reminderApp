f5833c2fcd3c4b88679065eb32782bae54d87df1
ahmedraza <ahmed.raza1054@gmail.com>
2023-11-17 17:24:37 +0500

    code push

--- app.js
+++ app.js
@@ -18,3 +18,20 @@ function singUp() {
   localStorage.setItem("storeDetails", JSON.stringify(storData));
 }
 // Main page timer
+
+var date = document.getElementById("date");
+var clockDate = moment().format("LL");
+date.innerHTML = clockDate;
+
+var time = document.getElementById("time");
+setInterval(() => {
+  var currentTime = moment().format("LTS");
+  time.innerHTML = currentTime;
+}, 1000);
+
+var settime = "6:50:30 a";
+var timing = moment().format(settime);
+console.log("varible check ==>", timing);
+
+var fajar = document.getElementById("fajar");
+fajar.innerHTML = timing;
