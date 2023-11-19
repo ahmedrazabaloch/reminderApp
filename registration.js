67e0bc8d80e1ac37d2faf5e15f0afcbd74647c55
ahmedraza <ahmed.raza1054@gmail.com>
2023-11-20 02:33:01 +0500

    PWA added

--- registration.js
+++ registration.js
@@ -1,3 +1,14 @@
+// PWA Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(function (registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch(function (error) {
+      console.error("Service Worker registration failed:", error);
+    });
+}
 // Email Validition
 const emailRegex =
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
---
+++ service-worker.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/path/to/service-worker.js")
+    .then(function (registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch(function (error) {
+      console.error("Service Worker registration failed:", error);
+    });
+}
